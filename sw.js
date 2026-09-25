31b5dc07aa49af21c27c656e8f00740da11f8f14
wcscomponentes1-eng <wcscomponentes1@gmail.com>
2026-09-25 16:55:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wcs-geradores-v4';
+const CACHE_NAME = 'wcs-geradores-v5';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
